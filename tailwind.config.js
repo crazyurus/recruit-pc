1ea270fdbf2a584908ee20622940061b08801a27
Crazy Urus <crazyurus@vip.qq.com>
2022-08-31 12:44:53 +0800

    refactor: 目录结构调整

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+  ],
+  theme: {
+    colors: {
+      black: '#1f2329',
+      white: '#fff',
+    },
+  },
+  plugins: [],
+};
