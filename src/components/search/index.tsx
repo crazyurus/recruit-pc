4d948c7371f330e0557d7ba9039ba08182e9f68f
Crazy Urus <crazyurus@vip.qq.com>
2022-09-09 16:58:04 +0800

    refactor: 搜索使用 inputmode

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -11,12 +11,12 @@ function Search(): JSX.Element {
   };
 
   return (
-    <form className={classNames(styles.search, 'relative')} action="." onSubmit={e => e.preventDefault()}>
+    <div className={classNames(styles.search, 'relative')}>
       <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
         <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
-      <input type="search" className="block shadow p-2 pl-10 w-full text-sm text-gray-900 bg-gray-100 rounded-lg focus:bg-white" autoComplete="off" placeholder="搜索" onChange={handleChange} />
-    </form>
+      <input type="search" inputMode="search" className="block shadow p-2 pl-10 w-full text-sm text-gray-900 bg-gray-100 rounded-lg focus:bg-white" autoComplete="off" placeholder="搜索" onChange={handleChange} />
+    </div>
   );
 }
 
